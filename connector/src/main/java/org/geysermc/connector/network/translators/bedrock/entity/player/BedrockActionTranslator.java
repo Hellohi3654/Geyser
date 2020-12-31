1c85e7fc41f81e561506dd6ebf40881f233860a6
Hellohi3654 <57390764+Hellohi3654@users.noreply.github.com>
2021-01-01 02:37:00 +1100

    Update BedrockActionTranslator.java

--- connector/src/main/java/org/geysermc/connector/network/translators/bedrock/entity/player/BedrockActionTranslator.java
+++ connector/src/main/java/org/geysermc/connector/network/translators/bedrock/entity/player/BedrockActionTranslator.java
@@ -139,7 +139,7 @@ public class BedrockActionTranslator extends PacketTranslator<PlayerActionPacket
                         startBreak.setType(LevelEventType.BLOCK_START_BREAK);
                         startBreak.setPosition(vector.toFloat());
                         PlayerInventory inventory = session.getPlayerInventory();
-                        GeyserItemStack item = inventory.getItemInHand().sub(1);
+                        GeyserItemStack item = null;
                         ItemEntry itemEntry = null;
                         CompoundTag nbtData = new CompoundTag("");
                         if (item != null) {
