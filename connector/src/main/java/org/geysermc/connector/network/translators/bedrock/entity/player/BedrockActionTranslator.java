f0dfd3655d4d9edfda7d97be984d17435373382d
Hellohi3654 <57390764+Hellohi3654@users.noreply.github.com>
2021-01-10 13:47:18 +1100

    Update BedrockActionTranslator.java

--- connector/src/main/java/org/geysermc/connector/network/translators/bedrock/entity/player/BedrockActionTranslator.java
+++ connector/src/main/java/org/geysermc/connector/network/translators/bedrock/entity/player/BedrockActionTranslator.java
@@ -61,11 +61,6 @@ public class BedrockActionTranslator extends PacketTranslator<PlayerActionPacket
     public void translate(PlayerActionPacket packet, GeyserSession session) {
         Entity entity = session.getPlayerEntity();
 
-        // Send book update before any player action
-        if (packet.getAction() != PlayerActionPacket.Action.RESPAWN) {
-            session.getBookEditCache().checkForSend();
-        }
-
         Vector3i vector = packet.getBlockPosition();
         Position position = new Position(vector.getX(), vector.getY(), vector.getZ());
 
