b4aa37db4bf267003474a331978e7cc0f4558cda
Hellohi3654 <57390764+Hellohi3654@users.noreply.github.com>
2021-02-14 18:06:15 +1100

    Update BedrockNetworkStackLatencyTranslator.java

--- connector/src/main/java/org/geysermc/connector/network/translators/bedrock/BedrockNetworkStackLatencyTranslator.java
+++ connector/src/main/java/org/geysermc/connector/network/translators/bedrock/BedrockNetworkStackLatencyTranslator.java
@@ -79,19 +79,5 @@ public class BedrockNetworkStackLatencyTranslator extends PacketTranslator<Netwo
         session.getConnector().getGeneralThreadPool().schedule(
                 () -> session.sendUpstreamPacket(attributesPacket),
                 500, TimeUnit.MILLISECONDS);
-
-        if (session.getConnector().getConfig().isForwardPlayerPing()) {
-            long pingId;
-            // so apparently, as of 1.16.200
-            // PS4 divides the network stack latency timestamp FOR US!!!
-            // WTF
-            if (session.getClientData().getDeviceOS().equals(DeviceOS.NX)) {
-                // Ignore the weird DeviceOS, our order is wrong and will be fixed in Floodgate 2.0
-                pingId = packet.getTimestamp();
-            } else {
-                pingId = packet.getTimestamp() / 1000;
-            }
-            session.sendDownstreamPacket(new ClientKeepAlivePacket(pingId));
-        }
     }
 }
